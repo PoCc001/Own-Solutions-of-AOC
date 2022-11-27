45e16d5302e464e39f10e104449912fb38f0f202
PoCc001 <49920248+PoCc001@users.noreply.github.com>
2022-11-27 13:03:54 +0100

    Make header file usable for both C++ and C

--- ReadTXTFile.h
+++ ReadTXTFile.h
@@ -1,50 +1,243 @@
 #ifndef READTXTFILE_H
 #define READTXTFILE_H
 
+#ifdef __cplusplus
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
 
 namespace aoc {
-	std::vector<std::string> fileToStringArray(const std::string& name) {
-		std::ifstream in(name, std::ios::in);
+    typedef std::vector<std::string> stringArray;
+    
+    stringArray fileToStringArray(const std::string& name) {
+        std::ifstream in(name, std::ios::in);
+        
+        if (!in) {
+            std::cout << "File \"" << name << "\" doesn't exist!\nAbort!" << std::endl;
+            in.close();
+            exit(1);
+        } else {
+            std::size_t s = 100;
+            stringArray v{};
+            v.reserve(s);
+            v.resize(1);
+            
+            while (std::getline(in, v[v.size() - 1])) {
+                v.resize(v.size() + 1);
+            }
+            
+            in.close();
+            v.resize(v.size() - 1);
+            return v;
+        }
+    }
+    
+    std::string substrIndizes(const std::string& str, std::size_t begin, std::size_t end) {
+        if (end < begin) {
+            std::cout << "Second bound is smaller than the first one!" << std::endl;
+            exit(1);
+        }
+        
+        return str.substr(begin, end - begin);
+    }
 
-		if (!in) {
-			std::cout << "File \"" << name << "\" doesn't exist!\nAbort!" << std::endl;
-			in.close();
-			std::system("PAUSE");
-			exit(1);
-		}
-		else {
-			std::size_t s = 100;
-			std::vector<std::string> v{};
-			v.reserve(s);
-			v.resize(1);
+    stringArray divideString(const std::string& str, const std::string& del) {
+        stringArray arr{};
+        std::string sub = str;
+        std::size_t p = sub.find_first_of(del);
+        while (p != std::string::npos) {
+            arr.push_back(substrIndizes(sub, 0, p));
+            sub = sub.substr(p + del.length());
+            p = sub.find_first_of(del);
+        }
 
-			while (std::getline(in, v[v.size() - 1])) {
-				v.resize(v.size() + 1);
-			}
-			
-			in.close();
+        if (sub.length() > 0) {
+            arr.push_back(sub);
+        }
 
-			v.resize(v.size() - 1);
-
-			return v;
-		}
-	}
-
-	std::string substrIndizes(const std::string& str, std::size_t begin, std::size_t end) {
-		if (end < begin) {
-			std::cout << "Second bound is smaller than the first one!" << std::endl;
-			std::system("PAUSE");
-			exit(1);
-		}
-
-		return str.substr(begin, end - begin);
-	}
-
-	typedef std::vector<std::string> stringArray;
+        return arr;
+    }
 }
+#else
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct stringArrayType {
+    size_t rows;
+    char** arr;
+} stringArray;
+
+stringArray fileToStringArray(const char* name) {
+    FILE* file = fopen(name, "r");
+    
+    if (file == NULL) {
+        printf("File \"%s\" doesn't exist!\nAbort!\n", name);
+        exit(1);
+    } else {
+        size_t byteSize = 0;
+        size_t lines = 1;
+        char c = fgetc(file);
+        
+        while (c != EOF) {
+            byteSize++;
+            
+            if (c == '\n') {
+                lines++;
+            }
+            
+            c = fgetc(file);
+        }
+        
+        if (fseek(file, 0, SEEK_SET)) {
+            puts("Seeking unsuccessful!\nAbort!\n");
+            fclose(file);
+            exit(1);
+        }
+        
+        char* arr = (char*)(calloc(byteSize, sizeof(char)));
+        
+        c = fgetc(file);
+        size_t i = 0;
+        
+        while (c != EOF && i < byteSize) {
+            arr[i] = c;
+            i++;
+            c = fgetc(file);
+        }
+        
+        fclose(file);
+        
+        if (c != EOF || i < byteSize) {
+            puts("Was the file changed during reading?\nThat shouldn't happen anyways, right? - Right?\nAbort!\n");
+            free(arr);
+            exit(1);
+        }
+        
+        stringArray strArr = {lines, (char**)(calloc(lines, sizeof(char*)))};
+        
+        size_t gi = 0;
+        size_t rowSize = 0;
+        for (i = 0; i < lines; i++) {
+            rowSize = 0;
+            while (arr[gi] != '\n' && gi < byteSize) {
+                rowSize++;
+                gi++;
+            }
+            strArr.arr[i] = (char*)(calloc(rowSize + 1, sizeof(char)));
+        }
+        
+        gi = 0;
+        for (i = 0; i < lines; i++) {
+            size_t col = 0;
+            while (arr[gi] != '\n' && gi < byteSize) {
+                strArr.arr[i][col] = arr[gi];
+                col++;
+                gi++;
+            }
+
+            gi++;
+        }
+
+        free(arr);
+        
+        return strArr;
+    }
+}
+    
+void freeStringArray(stringArray* arr) {
+    if (arr != NULL) {
+        for (size_t i = 0; i < arr->rows; i++) {
+            free(arr->arr[i]);
+        }
+    
+        free(arr->arr);
+        arr->arr = NULL;
+        arr->rows = 0;
+    }
+}
+
+char* getString(const stringArray* strArr, size_t r) {
+    if (r >= strArr->rows) {
+        return NULL;
+    } else {
+        return strArr->arr[r];
+    }
+}
+
+char* getSubstring(const char* str, size_t begin, size_t end) {
+    if (str == NULL || begin > end) {
+        return NULL;
+    } else {
+        size_t i = 0;
+        while (str[i] != '\0' && i < begin) {
+            i++;
+        }
+
+        if (str[i] == '\0') {
+            return NULL;
+        }
+
+        while (str[i] != '\0' && i < end) {
+            i++;
+        }
+
+        char* substr = (char*)(calloc(i - begin + 2, sizeof(char)));
+
+        for (size_t j = 0; j < i - begin + 1; j++) {
+            substr[j] = str[begin + j];
+        }
+
+        return substr;
+    }
+}
+
+stringArray divideString(const char* str, const char* del) {
+        size_t dellength = 0;
+        while (del[dellength] != '\0') {
+            dellength++;
+        }
+
+        size_t stringCount = 1;
+        size_t i = 0;
+        while (str[i] != '\0') {
+            if (str[i] == del[0]) {
+                stringCount++;
+                i += dellength;
+            }
+            i++;
+        }
+
+        stringArray arr = {stringCount, (char**)(calloc(stringCount, sizeof(char*)))};
+
+        size_t gi = 0;
+        for (i = 0; i < stringCount; i++) {
+            size_t wordSize = 0;
+            while (str[gi] != del[0] && str[gi] != '\0') {
+                wordSize++;
+                gi++;
+            }
+
+            gi += dellength;
+
+            arr.arr[i] = (char*)(calloc(wordSize + 1, sizeof(char)));
+        }
+
+        gi = 0;
+
+        for (i = 0; i < stringCount; i++) {
+            size_t wordIndex = 0;
+            while (str[gi] != del[0] && str[gi] != '\0') {
+                arr.arr[i][wordIndex] = str[gi];
+                wordIndex++;
+                gi++;
+            }
+
+            gi += dellength;
+        }
+
+        return arr;
+    }
+#endif
 
 #endif
