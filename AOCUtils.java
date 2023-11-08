892056cfbeb1bb9af7e69165dfc6f1b060611256
PoCc001 <49920248+PoCc001@users.noreply.github.com>
2023-11-08 17:27:27 +0100

    add PriorityQueue implementation
    the inner class DynamicPriorityQueue can handle changes to its elements that affect their order, however it is not very efficient

--- AOCUtils.java
+++ AOCUtils.java
@@ -1,9 +1,11 @@
 package aocjava;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Collections;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class AOCUtils {
     public static List<String> fileToStringList(String fileName) {
@@ -32,8 +34,39 @@ public class AOCUtils {
                 }
             }
 
-            List<String> empty = Collections.emptyList();
-            return empty;
+            return Collections.emptyList();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException ioe) {
+                    System.out.println("Couldn't close BufferedReader!");
+                    ioe.printStackTrace();
+                    System.exit(1);
+                }
+            }
+        }
+    }
+
+    public static Stream<String> fileToStringStream(String fileName) {
+        File file = new File(fileName);
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(file));
+            return br.lines();
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException ioe) {
+                    System.out.println("Couldn't close BufferedReader!");
+                    ioe.printStackTrace();
+                    System.exit(1);
+                }
+            }
+
+            return Stream.of();
         } finally {
             if (br != null) {
                 try {
@@ -63,15 +96,17 @@ public class AOCUtils {
         return arr;
     }
 
-    public static List<String> divideString(String str, String del) {
+    public static List<String> divideString(final String str, String... del) {
         List<String> l = new ArrayList<>();
         String sub = str;
-        int p = str.indexOf(del);
+        String currDel = Arrays.stream(del).min(Comparator.comparingInt(str::indexOf)).orElse(null);
+        int p = currDel != null ? str.indexOf(currDel) : -1;
 
         while (p != -1) {
             l.add(sub.substring(0, p));
-            sub = sub.substring(p + del.length());
-            p = sub.indexOf(del);
+            sub = sub.substring(p + currDel.length());
+            currDel = Arrays.stream(del).min(Comparator.comparingInt(sub::indexOf)).get();
+            p = str.indexOf(currDel);
         }
 
         if (sub.length() > 0) {
@@ -79,5 +114,164 @@ public class AOCUtils {
         }
 
         return l;
+    }
+
+    public static IntStream stringsToInts(Stream<String> ss, String... dividers) {
+        if (dividers.length == 0) {
+            return ss.mapToInt(Integer::parseInt);
+        } else {
+            return ss.flatMapToInt(s -> divideString(s, dividers).stream().mapToInt(Integer::parseInt));
+        }
+    }
+
+    public static class DynamicPriorityQueue<T> extends PriorityQueue<T> {
+        private final List<T> uncompList;
+        private final List<Comparable<T>> compList;
+        private final Comparator<T> comparator;
+
+        public DynamicPriorityQueue(int initialCapacity, Comparator<T> c) {
+            if (c == null) {
+                throw new NullPointerException("Comparator must be non-null!");
+            }
+            compList = null;
+            uncompList = new ArrayList<>(initialCapacity);
+            comparator = c;
+        }
+
+        public DynamicPriorityQueue(int initialCapacity) {
+            compList = new ArrayList<>(initialCapacity);
+            uncompList = null;
+            comparator = null;
+        }
+
+        public DynamicPriorityQueue() {
+            this(0);
+        }
+
+        public DynamicPriorityQueue(Comparator<T> c) {
+            this(0, c);
+        }
+
+        @Override
+        public boolean add(T t) {
+            if (t == null) {
+                throw new NullPointerException("Cannot add null values!");
+            } else if (!(t instanceof Comparable) && comparator == null) {
+                throw new ClassCastException("comparator is null, but the type has no natural order!");
+            } else if (compList != null) {
+                compList.add((Comparable<T>)t);
+            } else {
+                uncompList.add(t);
+            }
+            return true;
+        }
+
+        @Override
+        public boolean offer(T t) {
+            return add(t);
+        }
+
+        @Override
+        public boolean remove(Object o) {
+            return compList == null ? uncompList.remove(o) : compList.remove(o);
+        }
+
+        @Override
+        public boolean contains(Object o) {
+            return compList == null ? uncompList.contains(o) : compList.contains(o);
+        }
+
+        @Override
+        public T peek() {
+            return compList == null ? uncompList.stream().min(comparator).orElse(null) : (T)compList.stream().min(Comparator.<Comparable>naturalOrder()).orElse(null);
+        }
+
+        @Override
+        public T poll() {
+            T t = peek();
+            remove(t);
+            return t;
+        }
+
+        @Override
+        public int size() {
+            return compList == null ? uncompList.size() : compList.size();
+        }
+
+        @Override
+        public Object[] toArray() {
+            return compList == null ? uncompList.toArray() : compList.toArray();
+        }
+
+        @Override
+        public <E> E[] toArray(E[] es) {
+            return compList == null ? uncompList.toArray(es) : compList.toArray(es);
+        }
+
+        @Override
+        public void clear() {
+            if (compList == null) {
+                uncompList.clear();
+            } else {
+                compList.clear();
+            }
+        }
+
+        @Override
+        public Iterator<T> iterator() {
+            return compList == null ? uncompList.iterator() : compList.stream().map(c -> (T)c).toList().iterator();
+        }
+
+        @Override
+        public Comparator<T> comparator() {
+            return comparator;
+        }
+
+        @Override
+        public boolean removeIf(Predicate<? super T> filter) {
+            if (compList == null) {
+                return uncompList.removeIf(filter);
+            } else {
+                boolean removedAtLeastOne = false;
+                for (Comparable<T> c : compList) {
+                    T t = (T)c;
+                    if (!filter.test(t)) {
+                        compList.remove(t);
+                        removedAtLeastOne = true;
+                    }
+                }
+                return true;
+            }
+        }
+
+        @Override
+        public boolean removeAll(Collection<?> c) {
+            if (c.contains(null)) {
+                throw new NullPointerException("One element in the collection is null!");
+            }
+            return compList == null ? uncompList.removeAll(c) : compList.removeAll(c);
+        }
+
+        @Override
+        public boolean retainAll(Collection<?> c) {
+            if (c.contains(null)) {
+                throw new NullPointerException("One element in the collection is null!");
+            }
+            return compList == null ? uncompList.retainAll(c) : compList.retainAll(c);
+        }
+
+        @Override
+        public void forEach(Consumer<? super T> action) {
+            if (action == null) {
+                throw new NullPointerException("Action is null!");
+            }
+            if (compList == null) {
+                uncompList.forEach(action);
+            } else {
+                for (Comparable<T> c : compList) {
+                    action.accept((T)c);
+                }
+            }
+        }
     }
 }
